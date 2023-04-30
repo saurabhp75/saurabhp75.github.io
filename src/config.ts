0b99dd9852852ee3862ec6dd961bffb3da27e694
Saurabh Prakash <saurabhp75@gmail.com>
2023-04-30 16:57:39 +0530

    Update config
    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://saurabhp75.github.io/",
+  author: "Saurabh Prakash",
+  desc: "Saurabh's personal website",
+  title: "SP",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
